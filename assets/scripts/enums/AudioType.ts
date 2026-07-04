925f2d43a47733092ed9b22c8f5970bbeb770a06
DESKTOP-RR6M8F2\PC <tientien187@gmail.com>
2026-07-04 12:04:54 +0700

    Update data

--- assets/scripts/enums/AudioType.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Enum phân loại âm thanh trong game.
- * AudioManager dùng để phát đúng kênh (SFX / Music / UI).
- */
-export enum AudioType {
-    SFX = 0,
-    MUSIC = 1,
-    UI = 2,
-}
--- assets/scripts/enums/GameMode.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Enum định nghĩa các chế độ chơi.
- */
-export enum GameMode {
-    TRIPLE_MATCH = 'TRIPLE_MATCH',
-    ORDER_MATCH = 'ORDER_MATCH',
-}
