6a7fc2824614434b56fc3d75467f2abc833dea0f
DESKTOP-RR6M8F2\PC <tientien187@gmail.com>
2026-07-15 12:27:00 +0700

    Update data

--- application.74029.js
+++ application.74029.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.a7f1a.json';
+          this.settingsPath = 'src/settings.e03af.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.8d59a.js
+++ index.8d59a.js
@@ -1,4 +1,4 @@
-System.register(["./application.415b3.js"], function (_export, _context) {
+System.register(["./application.74029.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
