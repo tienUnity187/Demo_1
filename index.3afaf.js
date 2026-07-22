24cd1730ed6b4267175226c9b4cedb6eda6466c2
DESKTOP-RR6M8F2\PC <tientien187@gmail.com>
2026-07-22 13:59:37 +0700

    Update data

--- index.3afaf.js
+++ index.3afaf.js
@@ -1,4 +1,4 @@
-System.register(["./application.bad0c.js"], function (_export, _context) {
+System.register(["./application.74eb2.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
