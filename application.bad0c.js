7285173dcb834c16100e79546ae66dd931a9a92a
DESKTOP-RR6M8F2\PC <tientien187@gmail.com>
2026-07-22 11:29:14 +0700

    Update data

--- application.bad0c.js
+++ application.bad0c.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.e03af.json';
+          this.settingsPath = 'src/settings.6f67e.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
